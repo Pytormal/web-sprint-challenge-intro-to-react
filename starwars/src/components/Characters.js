149e39160e2c26d301cc80e04fc4ec13706b163c
Alex Andrew <pytormal@live.com>
2020-08-30 18:11:19 -0700

    removed characters.css

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Character from "./Character";
-import "./Characters.css";
 
 const Characters = (props) => {
   const { characters } = props;
 
-  return <div className="character-text">rendered from characters.js</div>;
+  return <div>rendered from characters.js</div>;
 };
 
 export default Characters;
